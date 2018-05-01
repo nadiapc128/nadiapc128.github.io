3764081e20aca8f7789b115c7a2b39a65dca9a81
NadiaPC <bsbnadiapc@hotmail.com>
2018-04-30 23:17:53 -0500

    agregando tecnologías

--- js/init.js
+++ js/init.js
@@ -4,5 +4,16 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    $('.pushpin-demo-nav').each(function() {
+      var $this = $(this);
+      var $target = $('#' + $(this).attr('data-target'));
+      $this.pushpin({
+          top: $target.offset().top,
+          bottom: $target.offset().top + $target.outerHeight() - $this.height()
+      });
+    });
+
+      $('.materialboxed').materialbox();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
